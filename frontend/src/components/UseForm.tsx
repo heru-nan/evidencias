547225609465f6a0b46aae279a80d6b949af9403
leoxz98 <leoxz98@gmail.com>
2022-11-16 11:32:57 -0300

    fix onchange evet checkbox

--- frontend/src/components/UseForm.tsx
+++ frontend/src/components/UseForm.tsx
@@ -4,9 +4,13 @@ export const UseForm = (callback: any, initialState = {}) => {
   const [values, setValues] = useState(initialState);
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  
     if (event.target.type === "checkbox")
       setValues({ ...values, [event.target.name]: event.target.checked });
-    setValues({ ...values, [event.target.name]: event.target.value });
+    else{
+      setValues({ ...values, [event.target.name]: event.target.value });
+    }
+    
   };
 
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
