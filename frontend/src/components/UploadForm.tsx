ce72c0e5619fdb3d9c36eb1723bc3f409e19dbb3
heru-nan <hrwgallardo@gmail.com>
2022-12-20 12:22:00 -0300

    add upload file form

--- frontend/src/components/UploadForm.tsx
+++ frontend/src/components/UploadForm.tsx
@@ -1,91 +1,98 @@
-import React, {useState} from "react"
-import {UseForm} from "./UseForm"
-import {Button,Form,FormGroup,Label,Input,Col,Alert} from "reactstrap"
+import React, { useState } from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Col,
+  Container,
+} from "reactstrap";
 
+import axios from "axios";
+import Swal from "sweetalert2";
 
+const UploadForm = () => {
+  const [name, setName] = useState("");
+  const [inputFiles, setInputFiles] = useState(null);
 
-const UploadForm = () =>{
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (name && inputFiles) {
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("file", inputFiles);
 
-    const initialState = {
-        nombre: "",
-        id_fk_pub: 0,
+      axios
+        .post("http://localhost:5000/api/files", formData, {
+          headers: { "Content-type": "multipart/form-data" },
+        })
+        .then(({ data }) => {
+          console.log(data);
+          if (!data.error)
+            Swal.fire({
+              title: "Archivo subido correctamente",
+              icon: "success",
+              showConfirmButton: false,
+              timer: 2000,
+            });
+          else
+            Swal.fire({
+              title: "Error Interno",
+              icon: "error",
+              showConfirmButton: false,
+              timer: 2000,
+            });
+        })
+        .catch((e) => console.log(e));
+    } else {
+      Swal.fire({
+        title: "Error al ingresar datos",
+        icon: "error",
+        showConfirmButton: false,
+        timer: 2000,
+      });
     }
+  };
 
-    async function formularioCallback(){
-        console.log(values)
-        console.log(inputFiles)
-        const formData = new FormData()
-        formData.append("values.nombre",inputFiles)
-        //hacer fetch a values.nombre e input files
-        /*
-        fetch("http://localhost:5000/api/files", {
-            method: 'POST',
-            body: formData
-          })
-          .then(response => response.json())
-          .then(data => {
-            console.log(data)
-          })
-          .catch(error => {
-            console.error(error)
-          })
-        }
-        */
+  return (
+    <Container>
+      <h4 className="my-4">Subir Archivos</h4>
 
-        alert("subido correctamente");
-        
-        
-        // aqui va lo del mandar a backend y revisar que todo este bien??
-    }
+      <Form onSubmit={onSubmit}>
+        <FormGroup row>
+          <Label sm={2}>Nombre</Label>
+          <Col sm={9}>
+            <input
+              className="form-control"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Ingresa el nombre del archivo"
+            />
+          </Col>
+        </FormGroup>
 
-    const {onChange,onSubmit,values} = UseForm(formularioCallback,initialState)
+        <FormGroup row>
+          <Label sm={2}>Archivo</Label>
+          <Col sm={9}>
+            <Input
+              id="archivo"
+              name="archivo"
+              type="file"
+              single
+              onChange={(e) => setInputFiles(e.target.files[0])}
+              required
+            />
+          </Col>
+        </FormGroup>
 
-    const [inputFiles, setInputFiles] = React.useState(null)
+        <Button type="submit" color="primary">
+          Subir
+        </Button>
+      </Form>
+    </Container>
+  );
+};
 
-
-    return(
-        <Form onSubmit={onSubmit}>
-
-            <FormGroup>
-                <Label htmlFor="head-form"></Label>
-                Subir archivos
-            </FormGroup>
-
-            <FormGroup row>
-                <Label sm={2}>Nombre</Label>
-                <Col sm={9}>
-                    <Input
-                        id= "nombre"
-                        name = "nombre"
-                        placeholder="Ingresar nombre del archivo"
-                        required
-                        
-                        onChange = {onChange}
-                    />
-                </Col>
-            </FormGroup>
-
-            <FormGroup row>
-                <Label sm={2}>Archivo</Label>
-                <Col sm={9}>
-                    <Input
-                        id= "archivo"
-                        name = "archivo"
-                        type="file"
-                        single
-                        onChange={(e) => setInputFiles(e.target.files)}
-                        required
-                    />
-                </Col>
-            </FormGroup>
-
-            <Button type="submit" color="primary">
-                Subir
-            </Button>
-
-
-        </Form>
-    )
-}
-
-export default UploadForm
+export default UploadForm;
