fe447699049b908845485af293152f3d93b6806f
heru-nan <hrwgallardo@gmail.com>
2023-01-04 10:50:21 -0300

    add cypress and unit test

---
+++ frontend/src/components/Publicaciones.cy.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Publicaciones from "./Publicaciones";
+
+describe("<Publicaciones />", () => {
+  it("renders", () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Publicaciones />);
+
+    // eslint-disable-next-line no-sparse-arrays
+    const tableRows = [
+      ,
+      "ID",
+      "Autores",
+      "Titulo",
+      "Revisa",
+      "Indexación",
+      "Fecha",
+      "publicacion",
+      "Autores Extranjeros",
+      "Clasificación",
+      "Disciplina",
+      "ISSN/DOI",
+      "Acciones",
+    ];
+
+    tableRows.forEach((row) => {
+      cy.contains(row);
+    });
+  });
+});
